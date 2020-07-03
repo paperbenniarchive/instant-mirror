25c1c9e65d08d36a8c2c417dad8195703ec39502
Daniël <daniel@lngzl.nl>
2020-07-03 18:35:29 +0200

    :sparkles: automatic

--- src/app.js
+++ src/app.js
@@ -6,16 +6,19 @@ const { download } = require('./utils/download')
 
 const base = 'http://instantos.surge.sh/'
 
-rp(base)
-  .then(function (html) {
-    $('a', html).each((i, elm) => {
-      const attribute = elm.attribs.href
-      if (attribute.startsWith('.') || attribute.startsWith('http')) return
-      download(base + attribute, attribute, () => {
-        console.log('a')
+const minutes = 15; var minutesInMs = minutes * 60 * 1000
+setInterval(function () {
+  rp(base)
+    .then(function (html) {
+      $('a', html).each((i, elm) => {
+        const attribute = elm.attribs.href
+        if (attribute.startsWith('.') || attribute.startsWith('http')) return
+        download(base + attribute, attribute, () => {
+          console.log('Downloaded: ' + attribute)
+        })
       })
     })
-  })
-  .catch(function (err) {
-    console.error(err)
-  })
+    .catch(function (err) {
+      console.error(err)
+    })
+}, minutesInMs)
